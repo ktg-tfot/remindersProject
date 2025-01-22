631560c7ee48cf8b1080a2d9a60987a6b9f78d97
ktg <ktg.tfot@gmail.com>
2025-01-22 17:14:40 +0900

    first commit

---
+++ remindersProject/remindersProject/remindersProjectApp.swift
@@ -0,0 +1,32 @@
+//
+//  remindersProjectApp.swift
+//  remindersProject
+//
+//  Created by 김태건 on 1/22/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct remindersProjectApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}

414e2a7c1a08bf3f58f84cc5582ad2106438dafb
ktg <ktg.tfot@gmail.com>
2025-01-24 03:17:32 +0900

    [메인]편집 버튼, 검색창 추가

--- remindersProject/remindersProject/ContentView.swift
+++ remindersProject/remindersProject/ContentView.swift
@@ -11,51 +11,65 @@ import SwiftData
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
     @Query private var items: [Item]
-
+    @State private var editMode: EditMode = .inactive
+    @State private var searchText = ""
+    
+    //오늘 예정 전체 완료
+    
     var body: some View {
-        NavigationSplitView {
+        NavigationStack {
             List {
                 ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
-        }
-    }
 
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
+                }
             }
+            .environment(\.editMode, $editMode)
+            .navigationBarItems(trailing: editButton)
+            //            .toolbar {
+            //                ToolbarItem(placement: .navigationBarTrailing) {
+            //                    EditButton()
+            //                }
+            .searchable(text: $searchText, prompt: "검색")    //Check B
+//            .navigationTitle("Reminders")
+            
+        }
+        
+    }
+    
+    //Check A
+    private var editButton: some View {
+        return Button {
+            if editMode == .inactive {
+                editMode = .active
+            } else {
+                editMode = .inactive
+            }
+        } label: {
+            Text(editMode == .inactive ? "편집" : "완료")
         }
     }
 }
+    
+
+
+
+private func addItem() {
+    //            withAnimation {
+    //                let newItem = Item(timestamp: Date())
+    //                modelContext.insert(newItem)
+    //            }
+}
+
+//    private func deleteItems(offsets: IndexSet) {
+//        withAnimation {
+//            for index in offsets {
+//                modelContext.delete(items[index])
+//            }
+//        }
+//    }
+//}
 
 #Preview {
     ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
+    //        .modelContainer(for: Item.self, inMemory: true)
 }
--- remindersProject/remindersProject/Item.swift
+++ remindersProject/remindersProject/Item.swift
@@ -10,9 +10,18 @@ import SwiftData
 
 @Model
 final class Item {
-    var timestamp: Date
+    //알림 id
+    var id: String = UUID().uuidString
+    //알림 제목
+    var name: String
+    //알림 내용
+    var content: String
+    //알림 생성 날짜
+    var createdAt: Date
     
-    init(timestamp: Date) {
-        self.timestamp = timestamp
+    init(name: String, content: String, createdAt: Date = Date()) {
+        self.name = ""
+        self.content = ""
+        self.createdAt = createdAt
     }
 }
